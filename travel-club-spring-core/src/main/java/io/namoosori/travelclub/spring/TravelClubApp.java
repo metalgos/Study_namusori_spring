f7511578ca13d8dfd557bf2dd0c5a77181a98011
김용선 <metalgos2@gmail.com>
2024-01-20 23:22:52 +0900

    멤버서비스 로직 완성후 메인에서 사용

--- travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/TravelClubApp.java
+++ travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/TravelClubApp.java
@@ -1,7 +1,10 @@
 package io.namoosori.travelclub.spring;
 
+import io.namoosori.travelclub.spring.aggregate.club.CommunityMember;
 import io.namoosori.travelclub.spring.aggregate.club.TravelClub;
 import io.namoosori.travelclub.spring.service.ClubService;
+import io.namoosori.travelclub.spring.service.MemberService;
+import io.namoosori.travelclub.spring.service.sdo.MemberCdo;
 import io.namoosori.travelclub.spring.service.sdo.TravelClubCdo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,14 +15,24 @@ import java.util.Date;
 
 //테스트용 메인클래스
 public class TravelClubApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //xml파일을 가져와서 컨텍스트 생성, xml을 가져오는 클래스패스 사용
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        String [] beanNames= context.getBeanDefinitionNames();
-        System.out.println(Arrays.toString(beanNames));
-//
+        String[] beanNames = context.getBeanDefinitionNames();
+        //System.out.println(Arrays.toString(beanNames));
+
+        MemberService memberService = context.getBean("memeberServiceLogic", MemberService.class);
+
+        String memberId = memberService.registerMember(
+                new MemberCdo("test@nestree.io","kim","test member", "010-2222-2222","2020.10.20")
+        );
+        CommunityMember foundedMember = memberService.findMemberById(memberId);
+
+        System.out.println(foundedMember.toString());
+
+
 //        TravelClubCdo clubCdo = new TravelClubCdo("TravelClub", "Test TravelClub");
 //        ClubService clubService = context.getBean("clubService", ClubService.class);
 //
