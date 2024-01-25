ee5609cb80f1323f03bb8d7f90f5309a6f1b324b
김용선 <metalgos2@gmail.com>
2024-01-26 03:27:12 +0900

    여러 매핑방법 테스트

--- travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
+++ travelclub-spring-web/src/main/java/io/namoosori/travelclub/web/controller/ClubController.java
@@ -1,12 +1,12 @@
 package io.namoosori.travelclub.web.controller;
 
+import io.namoosori.travelclub.web.aggregate.club.TravelClub;
 import io.namoosori.travelclub.web.service.ClubService;
 import io.namoosori.travelclub.web.service.sdo.TravelClubCdo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClubController {
@@ -21,5 +21,16 @@ public class ClubController {
     public String register(@RequestBody TravelClubCdo travelClubCdo){
 
         return clubService.registerClub(travelClubCdo);
+    }
+    @GetMapping("/club/all")
+    public List<TravelClub> findAll(){
+
+        return clubService.findAll();
+    }
+
+    @GetMapping("/club/{clubId}")
+    public TravelClub find(@PathVariable String clubId){
+
+        return clubService.findClubById(clubId);
     }
 }
