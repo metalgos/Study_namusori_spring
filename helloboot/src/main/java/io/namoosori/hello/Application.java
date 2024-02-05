a8dd2b7de7fabf3c7f007b29755f64673e4bbab6
김용선 <metalgos2@gmail.com>
2024-02-06 05:22:17 +0900

    폴더 리팩토링

--- helloboot/src/main/java/io/namoosori/hello/Application.java
+++ helloboot/src/main/java/io/namoosori/hello/Application.java
@@ -1,4 +1,4 @@
-package org.example;
+package io.namoosori.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- helloboot/src/main/java/io/namoosori/hello/controller/HelloController.java
+++ helloboot/src/main/java/io/namoosori/hello/controller/HelloController.java
@@ -1,8 +1,16 @@
 package io.namoosori.hello.controller;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HelloController {
+
+    @GetMapping("/")
+    public String come(){
+        return "Hello Spring Boot";
+    }
 
     @GetMapping("/hello")
     public String hello(){
